4a677c80e2214cf825fc650fdbc689831734c0de
snrnapa <shino.satoru@gmail.com>
2024-02-06 00:32:53 +0900

    javaを用いてテストデータを作成した

--- demo/src/main/java/com/example/demo/controller/WorkerController.java
+++ demo/src/main/java/com/example/demo/controller/WorkerController.java
@@ -1,6 +1,8 @@
 package com.example.demo.controller;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,6 +20,22 @@ public class WorkerController {
 
     @GetMapping("/test")
     public void createTestData() {
+        List<Worker> testWorkerList = new ArrayList<Worker>();
+        for (int i = 0; i < 1000; i++) {
+            Worker worker = new Worker();
+            worker.setId(i);
+            worker.setName("UserName" + i);
+
+            Random rand = new Random();
+            int weight = rand.nextInt(30) + 35;
+            worker.setWeight(weight);
+            int teamid = rand.nextInt(4) + 0;
+            worker.setTeam_id(teamid);
+            testWorkerList.add(worker);
+
+        }
+
+        workerRepository.saveAll(testWorkerList);
 
     }
 
@@ -26,13 +44,7 @@ public class WorkerController {
     public List<Worker> getAllWorker() {
 
         return workerRepository.findAll();
-        // Worker worker = new Worker();
-        // worker.setId(4);
-        // worker.setName("NatNatsumi");
-        // worker.setWeight(400);
-        // worker.setTeam_id(20);
 
-        // return worker;
     }
 
     @GetMapping("/user")
